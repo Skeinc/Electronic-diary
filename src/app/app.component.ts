0aaab2bc567212b7e6f1145aa24ba3d4d1e1b053
Anufriev Dmitry <skezydeveloper@gmail.com>
2024-03-25 22:16:54 +0300

    Complete base architecture, components, interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,7 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    template: '<app-application></app-application>',
 })
-export class AppComponent {
-  title = 'Electronic-diary';
-}
+export class AppComponent {}
