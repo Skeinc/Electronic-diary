9e3b4a850644024377bb8d8dad64a45a72bb8a55
Anufriev Dmitry <skezydeveloper@gmail.com>
2024-04-15 23:54:36 +0300

    Start doing personal cabinet of users

--- src/app/modules/personal/components/personal.component.ts
+++ src/app/modules/personal/components/personal.component.ts
@@ -9,6 +9,23 @@ export class PersonalComponent {
     // Определяет открыта ли меню
     isNavigationOpened: boolean = false;
 
+    // Значение нового пароля
+    newPasswordValue: string = '';
+
+    // Значение нового пароля (подтверждение)
+    confirmNewPasswordValue: string = '';
+
+    // Mocks
+    userData = {
+        id: 1,
+        surname: 'Ануфриев',
+        name: 'Дмитрий',
+        patronymic: 'Олегович',
+        email: 'iamskezy@gmail.com',
+        phone: '+79108765249',
+        password: 'qwerty123'
+    };
+
     // Метод скрывает/открывает меню
     onNavigationOpenedChange(isOpened: boolean) {
         this.isNavigationOpened = isOpened;
--- src/app/modules/personal/personal.module.ts
+++ src/app/modules/personal/personal.module.ts
@@ -3,6 +3,9 @@ import { PersonalComponent } from "./components/personal.component";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { NavigationModule } from "@shared/components/navigation/navigation.module";
+import { TextFieldModule } from "@shared/components/text-field/text-field.module";
+import { PasswordFieldModule } from "@shared/components/password-field/password-field.module";
+import { ButtonModule } from "@shared/components/button/button.module";
 
 @NgModule({
     declarations: [
@@ -13,6 +16,9 @@ import { NavigationModule } from "@shared/components/navigation/navigation.modul
         FormsModule,
         // Components
         NavigationModule,
+        TextFieldModule,
+        PasswordFieldModule,
+        ButtonModule,
     ],
     exports: [
         PersonalComponent,
