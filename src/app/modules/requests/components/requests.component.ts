75323a1b4444f08d53800e202c1e552b833ad49e
Anufriev Dmitry <skezydeveloper@gmail.com>
2024-04-21 22:00:32 +0300

    Request component

--- src/app/modules/requests/components/requests.component.ts
+++ src/app/modules/requests/components/requests.component.ts
@@ -1,8 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, HostListener, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-requests',
     templateUrl: './requests.component.html',
     styleUrl: './requests.component.scss',
 })
-export class RequestsComponent {}
+export class RequestsComponent implements OnInit{
+    // Определяет открыта ли меню
+    isNavigationOpened: boolean = false;
+
+    ngOnInit(): void {}
+
+    // Метод скрывает/открывает меню
+    onNavigationOpenedChange(isOpened: boolean) {
+        this.isNavigationOpened = isOpened;
+    }
+}
--- src/app/modules/requests/requests.module.ts
+++ src/app/modules/requests/requests.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RequestsComponent } from "./components/requests.component";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { NavigationModule } from "@shared/components/navigation/navigation.module";
 
 @NgModule({
     declarations: [
@@ -10,6 +11,8 @@ import { FormsModule } from "@angular/forms";
     imports: [
         CommonModule,
         FormsModule,
+        // Components
+        NavigationModule,
     ],
     exports: [
         RequestsComponent,
