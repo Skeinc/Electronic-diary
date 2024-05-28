57fd2baa9418d060ae746061d07fdfffbc63f457
Anufriev Dmitry <skezydeveloper@gmail.com>
2024-05-28 23:52:47 +0300

    Change number field component

--- src/app/shared/components/number-field/number-field.component.ts
+++ src/app/shared/components/number-field/number-field.component.ts
@@ -21,10 +21,10 @@ export class NumberFieldComponent {
     @Input() elementValue?: string | null = null;
 
     // Максимальное значение у поля
-    @Input() maxValue?: number | null = null;
+    @Input() maxValue: number = 999_999_999;
 
     // Минимальное значение у поля
-    @Input() minValue?: number | null = null;
+    @Input() minValue: number = -999_999_999;
 
     // Placeholder элемента
     @Input() elementPlaceholder?: string  | null = null;
@@ -52,4 +52,25 @@ export class NumberFieldComponent {
         return true;
     }
 
+    // Метод проверяет и корректирует значение
+    validateValue(value: string) {
+        let numValue = parseFloat(value);
+        if (numValue < this.minValue) {
+            numValue = this.minValue;
+        } else if (numValue > this.maxValue) {
+            numValue = this.maxValue;
+        }
+        this.elementValue = numValue.toString();
+        this.elementValueChange.emit(this.elementValue);
+    }
+
+    // Метод вызывается при изменении значения
+    onValueChange(value: string) {
+        this.validateValue(value);
+    }
+
+    // Метод вызывается при потере фокуса
+    onBlur() {
+        this.validateValue(this.elementValue || '');
+    }
 }
